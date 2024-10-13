263efe1a1dd67f6c248b94cd16d45f456316884b
farukwebcode21 <farukahmadmk@yahoo.com>
2024-10-13 09:50:46 +0600

    successfully web scraping done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 const url = "https://books.toscrape.com/";
 
 const main = async () => {
@@ -10,16 +11,45 @@ const main = async () => {
   await page.goto(url);
 
   const bookData = await page.evaluate((url) => {
+    const convertPrice = (price) => {
+      return parseFloat(price.replace("£", ""));
+    };
+
+    const convertRating = (rating) => {
+      switch (rating) {
+        case "one":
+          return 1;
+        case "Two":
+          return 2;
+        case "Three":
+          return 3;
+        case "Four":
+          return 4;
+        case "Five":
+          return 5;
+        default:
+          return 0;
+          break;
+      }
+    };
     const bookPods = Array.from(document.querySelectorAll(".product_pod"));
     const data = bookPods.map((book) => ({
       title: book.querySelector("h3 a").getAttribute("title"),
-      price: book.querySelector(".price_color").innerText,
+      price: convertPrice(book.querySelector(".price_color").innerText),
       imgSrc: url + book.querySelector("img").getAttribute("src"),
-      rating: book.querySelector(".star-rating").classList[1],
+      rating: convertRating(book.querySelector(".star-rating").classList[1]),
     }));
     return data;
   }, url);
-  console.log(bookData);
+
   await browser.close();
+
+  const saveData = () => {
+    fs.writeFile("data.json", JSON.stringify(bookData, null, 2), (err) => {
+      if (err) throw err;
+      console.log("Successfully saved JSON data");
+    });
+  };
+  saveData();
 };
 main();
